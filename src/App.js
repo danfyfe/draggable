3cf551dcb3385a27444bb38626156f2805652152
danfyfe <fyfedaniel@gmail.com>
2019-07-31 13:01:05 -0400

    draggable

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    draggables: [],
+    id: 1
+  }
+
+  dragStartHandler = (e) => {
+    // console.log('id',e.target.id)
+    e.dataTransfer.setData('text/plain', e.target.id)
+  }
+
+  dragOverHandler = (e) => {
+    // console.log("dragOver", e.target)
+    e.preventDefault()
+  }
+
+  dropHandler = (e) => {
+    e.preventDefault()
+    let data = e.dataTransfer.getData('text')
+    // console.log('data',data)
+    let draggableItem = document.getElementById(data)
+    if (e.target.id === 'target') {
+      draggableItem.style.backgroundColor = "red"
+      draggableItem.innerText = "This has been dropped"
+    } else {
+      draggableItem.style.backgroundColor = "#32cd32"
+      draggableItem.innerText = "This has been dropped BACK"
+    }
+    // console.log('e.target',e.target.id)
+    e.target.appendChild(draggableItem)
+
+    e.dataTransfer.clearData()
+  }
+
+  createNewDraggableBox = () => {
+    this.setState({
+      draggables: [...this.state.draggables, this.state.id],
+      id: this.state.id + 1
+    })
+  }
+
+  renderDraggableBoxes = () => {
+    return this.state.draggables.map(draggable => {
+      return <div key={draggable} className="draggable" id={draggable} draggable onDragStart={ this.dragStartHandler}>
+        This is draggable
+      </div>
+    })
+  }
+
+  render(){
+
+    return (
+      <>
+        <button onClick={this.createNewDraggableBox}>New Draggable</button>
+        <div className="draggablesContainer" id="draggablesContainer" onDragOver={this.dragOverHandler} onDrop={this.dropHandler}>
+          {this.renderDraggableBoxes()}
+        </div>
+        <div className="droppable" id="target" onDrop={this.dropHandler} onDragOver={this.dragOverHandler}>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
+
+// <div className="draggable" id={ id} draggable onDragStart={ this.dragStartHandler}>
+//   This is draggable
+// </div>
